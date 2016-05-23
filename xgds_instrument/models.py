630794fb921ae0156b6038c3e6c86d5d6abba624
Tamar Cohen <tamar.cohen@nasa.gov>
2016-05-23 12:53:33 -0700

    fixed note link urls

--- xgds_instrument/models.py
+++ xgds_instrument/models.py
@@ -86,15 +86,15 @@ class AbstractInstrumentDataProduct(models.Model):
         return reverse('instrument_data_csv',  kwargs={'productModel': self.modelAppLabel + '.' + self.modelTypeName,
                                                        'productPk': str(self.pk)})
 
-    @property
+    def thumbnail_url(self):
+        return None
+
     def thumbnail_time_url(self, event_time):
         return self.thumbnail_url()
 
-    @property
     def view_time_url(self, event_time):
         return self.view_url()
 
-    @property
     def view_url(self):
         return reverse('search_map_single_object', kwargs={'modelPK':self.pk,
                                                            'modelName': self.instrument.displayName})
